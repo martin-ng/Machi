a2e5bda41c92f3aaf27bb336d5c2b9c0f2fba0e9
Martin Ng <martinng1992@gmail.com>
2020-05-18 15:34:10 -0400

    implemented a few msg features

--- machi_bot.py
+++ machi_bot.py
@@ -1,10 +1,11 @@
 import discord
+import random
 from secrets import TOKEN
 from discord.ext import commands
 
-client = discord.Client()
-
-BOT_PREFIX = '.'
+# client = discord.Client()
+# BOT_PREFIX = '.'
+client = commands.Bot(command_prefix='.')
 
 
 @client.event
@@ -12,13 +13,36 @@ async def on_ready():
     print("Bot is ready to serve.")
 
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+    print(f'こんにちは, {member}')
+
+
+# @client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+    print(f'じゃあまたね, {member}')
+
+
+# @client.event
 async def on_message(message):
     message.content.lower()
     if message.author == client.user:
         return
-    if message.content.startswith("hello"):
-        await message.channel.send("I am going to take over the world and you will all become my munchkins")
+    if message.content.startswith("what"):
+        pass
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong')
+
+
+@client.command(aliases=['machi'])
+async def questions(ctx, *, question):
+    responses = ['I dunno, will you?', 'heh', 'likely!', 'no.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
 
 client.run(TOKEN)
-
-# client = commands.Bot(command_prefix='BOT_PREFIX')
